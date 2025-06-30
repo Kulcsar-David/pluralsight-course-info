fffc72887602a13cf34b0e62ddb103d0ad56877c
dkulcsar <kulcsardavid9292@gmail.com>
2025-06-30 18:59:49 +0200

    feat: setting up logger

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,7 +1,25 @@
 package com.pluralsight.courseinfo.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
     public static void main(String[] args) {
-        System.out.println("OK");
+        LOG.info("CourseRetrieve started!");
+        if (args.length == 0) {
+            LOG.warn("Please provide an author name as first argument!");
+            return;
+        }
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+            LOG.error("Unexpected Error");
+        }
+    }
+
+    private static void retrieveCourses(String authorId) {
+        LOG.info("Retrieved courses for author: '{}'", authorId);
     }
 }
